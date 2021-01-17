d6deb618ea5319cc5d982b3529b68866b68eec9e
Shiju Nambiar <skoodath@gmail.com>
2021-01-17 17:50:12 +0530

    Finishing up

--- script.js
+++ script.js
@@ -35,7 +35,9 @@ for (let i = 0; i < planets.length; i++){
 }
 
 const updatePlanetData = (data) =>{
+        
         const infoTitle = document.querySelector('.planet_card_title');
+        const infoImage = document.querySelector('.planet_details_img');
         const infoCircum = document.querySelector('.planet_details_circum');
         const infoPeriod = document.querySelector('.planet_details_orb_period');
         const infoSpeed = document.querySelector('.planet_details_orb_speed');
@@ -43,17 +45,16 @@ const updatePlanetData = (data) =>{
         const infoDistSun = document.querySelector('.planet_details_distancefromsun');
         const infoDistEarth = document.querySelector('.planet_details_distancefromearth');
         const infoTemp = document.querySelector('.planet_details_surfacetemp');
-
-        data.forEach(element => {
-            infoTitle.innerHTML = element.name;
-            infoCircum.innerHTML = `Circumference: ${element.circumference}`;
-            infoPeriod.innerHTML = `Orbit Period: ${element.orbit_period}`;
-            infoSpeed.innerHTML = `Orbit Speed: ${element.orbit_speed}`;
-            infoMoon.innerHTML = `No.of Moon: ${element.moons}`;
-            infoDistSun.innerHTML = `Distance from Sun: ${element.distancetosun}`;
-            infoDistEarth.innerHTML = `Distance from Earth: ${element.distancetoearth}`;
-            infoTemp.innerHTML = `Surface Temperature: ${element.avg_temp}`;
-    });
+            infoTitle.innerHTML = data[0].name;
+            infoImage.setAttribute('src', data[0].image);
+            infoImage.setAttribute('alt', data[0].name);
+            infoCircum.innerHTML = `Circumference: ${data[0].circumference}`;
+            infoPeriod.innerHTML = `Orbit Period: ${data[0].orbit_period}`;
+            infoSpeed.innerHTML = `Orbit Speed: ${data[0].orbit_speed}`;
+            infoMoon.innerHTML = `No.of Moon: ${data[0].moons}`;
+            infoDistSun.innerHTML = `Distance from Sun: ${data[0].distancetosun}`;
+            infoDistEarth.innerHTML = `Distance from Earth: ${data[0].distancetoearth}`;
+            infoTemp.innerHTML = `Surface Temperature: ${data[0].avg_temp}`;
 }
 
 closeInfo.addEventListener('click', ()=>{
